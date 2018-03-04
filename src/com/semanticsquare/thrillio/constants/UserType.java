8396b98b23a331b2dc527ee3ea85f38ad99213d6
rockysingh3 <rockygsingh@icloud.com>
2018-03-03 19:54:34 -0500

    updated

---
+++ src/com/semanticsquare/thrillio/constants/UserType.java
@@ -0,0 +1,12 @@
+package com.semanticsquare.thrillio.constants;
+
+public class UserType {
+	private UserType () {}
+	
+	
+//	these are run time constant so they can be captail 
+	public static final String USER = "user";
+	public static final String EDITOR = "user";
+	public static final String CHIEF_EDITOR = "user";
+
+}
--- src/com/semanticssquare/thrillio/manage/BookmarkManager.java
+++ src/com/semanticssquare/thrillio/manage/BookmarkManager.java
@@ -1,6 +1,8 @@
 package com.semanticssquare.thrillio.manage;
 
+import com.semanticssquare.thrillio.entites.Book;
 import com.semanticssquare.thrillio.entites.Movie;
+import com.semanticssquare.thrillio.entites.WebLink;
 
 public class BookmarkManager {
 	private static BookmarkManager instance = new BookmarkManager();
@@ -24,6 +26,29 @@ public class BookmarkManager {
 		
 		return movie;
 		
+	}
+	
+	public WebLink createWebLink(long id, String title, String url, String host) {
+		WebLink weblink = new WebLink();
+		weblink.setId(id);
+		weblink.setTitle(title);
+		weblink.setHost(host);
+		weblink.setUrl(url);
+		
+		return weblink;
+	}
+	
+	public Book createBook(long id, String title, int publicationYear, String publisher, double amazonRating, String[] authors, String genre) {
+		Book book = new Book();
+		book.setId(id);
+		book.setPublicationYear(publicationYear);
+		book.setTitle(title);
+		book.setPublisher(publisher);
+		book.setAmazonRating(amazonRating);
+		book.setAuthors(authors);
+		book.setGenre(genre);
+	
+		return book;
 	}
 
 }
